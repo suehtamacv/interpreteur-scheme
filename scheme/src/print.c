360713d461b281294fbb9ed0f9b87fbdf1cdf239
Matheus Cavalcante <matheus.cavalcante@ee.ufcg.edu.br>
2016-09-11 19:31:36 +0200

    Initial code

---
+++ scheme/src/print.c
@@ -0,0 +1,34 @@
+
+/**
+ * @file print.c
+ * @author François Cayre <cayre@yiking.(null)>
+ * @date Fri Jun 22 20:12:17 2012
+ * @brief Printing stuff for SFS.
+ *
+ * Printing stuff for SFS.
+ */
+
+#include "print.h"
+
+#include <stdio.h>
+
+void sfs_print_atom( object o ) {
+
+    return;
+}
+
+void sfs_print_pair( object o ) {
+
+    return;
+}
+
+void sfs_print( object o ) {
+
+    if ( SFS_PAIR == o->type ) {
+        sfs_print_pair( o );
+    }
+    else {
+        sfs_print_atom( o );
+    }
+
+}
