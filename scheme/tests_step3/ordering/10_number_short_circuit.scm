1c20a94edec3c4cedf965e46fa0319cf88468a21
Matheus Cavalcante <matheus.cavalcante@ee.ufcg.edu.br>
2016-10-25 14:31:01 +0200

    number equality

---
+++ scheme/tests_step3/ordering/10_number_short_circuit.scm
@@ -0,0 +1,8 @@
+;;; En tete pour script de test ;;;;;;;;;;
+; TEST_RETURN_CODE=PASS
+; TEST_COMMENT=Tests number equality with less than two arguments
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(= 1 1)
+(= 1 2)
+(= 1 2 'CaNExistePas)
