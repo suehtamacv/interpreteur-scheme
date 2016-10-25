8685082da08680ab63fa0bae3b97bb91909e1697
Matheus Cavalcante <matheus.cavalcante@ee.ufcg.edu.br>
2016-10-25 13:17:31 +0200

    ordering function tests

---
+++ scheme/tests_step3/ordering/03_less_than_arguments.scm
@@ -0,0 +1,13 @@
+;;; En tete pour script de test ;;;;;;;;;;
+; TEST_RETURN_CODE=PASS
+; TEST_COMMENT=Tests less than with less than two arguments
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(<)
+(< 1)
+(< #t)
+(< #\a)
+(< '())
+(< #\space)
+(< 'CaNExistePas)
+(< "Should work!")
