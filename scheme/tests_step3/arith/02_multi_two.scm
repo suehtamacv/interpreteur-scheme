8d2a9df3dc7702ffebd8513933f828d0fba440e4
taurus22294 <leanhdung0205@gmail.com>
2016-11-06 16:16:13 +0100

    test complexe avec plus, multi, minus

--- scheme/tests_step3/arith/02_multi_two.scm
+++ scheme/tests_step3/arith/02_multi_two.scm
@@ -1,6 +1,6 @@
 ;;; En tete pour script de test ;;;;;;;;;;
 ; TEST_RETURN_CODE=PASS
-; TEST_COMMENT=Tests if all the ordering functions are implemented 
+; TEST_COMMENT=Tests "*" which has 2 arguments 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (* 2 3)
